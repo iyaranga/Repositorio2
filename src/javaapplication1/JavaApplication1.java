c3af045008bfc407f519014724e96c1414d2c38d
ipyv <ipyv@DESKTOP-G8KPDAP>
2025-01-21 17:48:59 -0500

    Carga Inicial

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -13,6 +13,7 @@ public class JavaApplication1 {
 
     public static void main(String[] args) {
        System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
     }
     
 }
